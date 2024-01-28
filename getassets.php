d0ab211a61239083f8cdb1a9be9bd98cec05da3d
maderami <sdsoerty6584@gmail.com>
2024-01-28 13:45:51 +0600

    df

---
+++ getassets.php
@@ -0,0 +1,13 @@
+<?php
+require_once ('./assets/css/grid.css');
+require_once ('./assets/css/ie.css');
+require_once ('./assets/css/reset.css');
+require_once ('./assets/css/style.css');
+
+require_once ('./assets/js/cufon-replace.js');
+require_once ('./assets/js/cufon-yui.js');
+require_once ('./assets/js/FF-cash.js');
+require_once ('./assets/js/html5.js');
+require_once ('./assets/js/jquery-1.6.2.min.js');
+require_once ('./assets/js/Vegur_300.font.js');
+require_once ('./assets/js/Vegur_400.font.js');
